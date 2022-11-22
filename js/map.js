1c3d1e8b436bfb7c9115e21ffcd8489fb0ce9010
kharper2 <kharper2@hawk.iit.edu>
2022-11-21 21:29:57 -0600

    changed title

--- js/map.js
+++ js/map.js
@@ -1,32 +1,32 @@
 function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 51.250399331199496, lng: 22.56586883496531 },
-    zoom: 5,
-  });
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 51.250399331199496, lng: 22.56586883496531 },
+    zoom: 5,
+  });
 
 var marker = new google.maps.Marker({
-    position: { lat: 51.250399331199496, lng: 22.56586883496531 },
-    map: map,
-  });
+    position: { lat: 51.250399331199496, lng: 22.56586883496531 },
+    map: map,
+  });
 
 var infowindow = new google.maps.InfoWindow({
-    content:"This is they city from which my family is from!"
-  });
+    content:"This is they city from which my family is from!"
+  });
 
 var flightPlanCoordinates = [
-    { lat: 41.978611, lng: -87.904724 },
-    { lat: 52.1595, lng: 20.9670 },
-  ];
+    { lat: 41.978611, lng: -87.904724 },
+    { lat: 52.1595, lng: 20.9670 },
+  ];
 
 var flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: "#FF0000",
-    strokeOpacity: 1.0,
-    strokeWeight: 2,
-  });
+    path: flightPlanCoordinates,
+    geodesic: true,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+  });
 
-  flightPath.setMap(map);
+  flightPath.setMap(map);
 
 }
 
